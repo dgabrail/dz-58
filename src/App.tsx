90558c4f7e102556781ce6939babc1d7b6122250
Ramazanov Djabrail <ramazanovd612@gmail.com>
2022-11-15 11:20:25 +0600

    Выполнен второй уровень домашнего задания показываеться   Alert если функция передана кнопка отабражаеться если функция не передана соотеветствено кнопка не отображаеться

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import React, {useState} from 'react';
 import './App.css';
 import Modal from "./components/Modal/Modal";
+import Alert from "./components/Alert/Alert";
 
 
 function App() {
 
     const [showModal , setShowModal] = useState(false)
     const onClose = () => setShowModal(false)
+    const onDismiss = () => {}
 
   return (
     <div className="App">
+        <Alert onDismiss={onDismiss} type="danger"/>
+        <Alert type="success"/>
         <Modal show={showModal} onClose={onClose}/>
       <button className="btn btn-primary mt-5" onClick={() => setShowModal(true)}>
         Launch demo modal
